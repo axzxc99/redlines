98b8e4d93884c6d2f61d7426aaf8a768979c0089
axzxc99 <axzxc99@gmail.com>
2024-02-21 18:37:52 -0500

    Basic Site Structure and layout completed

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-	echo "♠";
-
+	//echo "♠";
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,5 +10,20 @@
 	</head>
 	<body onload="load()">
 		<div id="bg"></div>
+		<input type="search" id="topicSearch" placeholder="Search Topics" onkeydown="if(event.keyCode == 13) {searchTopics()}" name="topicSearch" form="searchForm" >
+		<div id="startSourceDiv">
+			<div class="startSource">
+				<p class="startTitle pSource">Start a topic with a <span>Primary Source</span></p>
+				<input type="button" value="+" onclick="sourcePrompt(true)">
+			</div><div class="startSource">
+				<p class="startTitle sSource">Start a topic with a <span>Secondary Source</span></p>
+				<input type="button" value="+" onclick="sourcePrompt(false)">
+			</div>
+		</div>
+		<div id="topicsPull">
+			<input style="" id="TPHandle" type="button" value="=" onclick="toggleTP()" data-toggled="false">
+			<div style="" id="TPMain"></div>
+		</div>
+		<form name="searchForm" id="searchForm" method="GET" action="res/php/search.php"></form>
 	</body>
 </html>
