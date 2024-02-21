e826b827925fec0563aaaa081bb97625493b11ed
axzxc99 <axzxc99@gmail.com>
2024-02-21 12:02:43 -0500

    adding structure

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 	echo "♠";
+
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Red Lines - Connect the Dots</title>
+		<link rel="stylesheet" type="text/css" href="res/misc/style.css">
+		<script src="res/js/init.js"></script>
+	</head>
+	<body onload="load()">
+		<div id="bg"></div>
+	</body>
+</html>
